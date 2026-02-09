7965a7962e899c951a87c3e8805685c1dc06d914
tommy <example@email.com>
2026-02-09 13:14:46 -0600

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./Home.css";
+
 
 export default function Home() {
   const navigate = useNavigate();
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -121,4 +121,4 @@ export default function MovieDetail() {
     </div>
   );
 }
-``
+
