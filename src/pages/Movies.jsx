ef42d846e37ed31df2cf5a5509ad7fdb7d4f35b1
tommy <example@email.com>
2026-02-09 11:54:35 -0600

    updated

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -19,7 +19,7 @@ export default function Movies() {
     const fetchMovies = async () => {
       setLoading(true);
       const res = await fetch(
-        `https://www.omdbapi.com/?apikey=${API_KEY=21463539&s}&s=${query}`
+        `https://www.omdbapi.com/?apikey==21463539&s${API_KEY}&s=${query}`
       );
       const data = await res.json();
 
