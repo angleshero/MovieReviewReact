a6bdc6d076adba42b2eadcf9ebde3b02fb8a1078
tommy <example@email.com>
2026-02-10 07:58:01 -0600

    update

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,9 +1,10 @@
 
 import React, { useEffect, useMemo, useState } from "react";
 import MovieCard from "../components/MovieCard.jsx";
+import assets from "../assets";
 
 
-function Movies() {
+function Movies( assets ) {
   const [movies, setMovies] = useState([]);
   const [status, setStatus] = useState("loading");
   const [error, setError] = useState(null);
@@ -12,7 +13,7 @@ function Movies() {
     <div className="moviesWrapper">
       <div className="movie">
         <figure className="movie__img--wrapper">
-          <img className="movie__img" src="./assets/BeforeSunrise.jpg" alt="" />
+          <img className="movie__img" src={assets.BeforeSunrise} alt="" />
         </figure>
         <div className="movie__title">
           Before Sunrise
@@ -27,7 +28,7 @@ function Movies() {
       </div>
       <div className="movie">
         <figure className="movie__img--wrapper">
-          <img className="movie__img" src="./assets/big-fish.jpg" alt="" />
+          <img className="movie__img" src={assets.BigFish} alt="" />
         </figure>
         <div className="movie__title">
           Big Fish
@@ -42,7 +43,7 @@ function Movies() {
       </div>
       <div className="movie">
         <figure className="movie__img--wrapper">
-          <img className="movie__img" src="./assets/ghostdog.jpg" alt="" />
+          <img className="movie__img" src={assets.GhostDog} alt="" />
         </figure>
         <div className="movie__title">
           Ghost Dog
@@ -57,7 +58,7 @@ function Movies() {
       </div>
       <div className="movie">
         <figure className="movie__img--wrapper">
-          <img className="movie__img" src="./assets/Mallrats.jpg" alt="" />
+          <img className="movie__img" src={assets.Mallrats} alt="" />
         </figure>
         <div className="movie__title">
           Mallrats
@@ -72,7 +73,7 @@ function Movies() {
       </div>
       <div className="movie">
         <figure className="movie__img--wrapper">
-          <img className="movie__img" src="./assets/MeteorMan.jpg" alt="" />
+          <img className="movie__img" src={assets.MeteorMan} alt="" />
         </figure>
         <div className="movie__title">
           Meteor Man
@@ -87,7 +88,7 @@ function Movies() {
       </div>
       <div className="movie">
         <figure className="movie__img--wrapper">
-          <img className="movie__img" src="./assets/BigHero6.jpg" alt="" />
+          <img className="movie__img" src={assets.BigHero6} alt="" />
         </figure>
         <div className="movie__title">
           Big Hero 6 
